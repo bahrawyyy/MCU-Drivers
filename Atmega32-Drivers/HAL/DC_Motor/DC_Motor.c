53364a740e2721d607d349ec4399f022b7dc2b45
Abdalla Elbahrawy <abdallabahrawyy@gmail.com>
2023-08-03 23:49:51 +0300

    DC Motor uploaded

--- Atmega32-Drivers/HAL/DC_Motor/DC_Motor.c
+++ Atmega32-Drivers/HAL/DC_Motor/DC_Motor.c
@@ -1,13 +1,14 @@
 /*
  * DC_Motor.c
  *
- *  Created on: Jul 23, 2023
+ *  Created on: Aug 3, 2023
  *      Author: Abdallah
  */
 
 
 #include "DC_Motor.h"
 #include "DC_MotorCfg.h"
+
 
 
 ES_t DC_Motor_Init(DC_MOTOR_Index Motor)
@@ -21,6 +22,15 @@ ES_t DC_Motor_Init(DC_MOTOR_Index Motor)
 	}else{
 		return ES_NOT_OK;
 	}
+
+
+	DIO_ES_tSetPinDirection(DIO_U8_PORT_B, DIO_U8_PIN_3, OUTPUT);
+	TIMER0_Config_t Timer0Cfg = {TIMER0_PHASE_CORRECT_MODE, TIMER0_PRESCALAR_8, OC0_NON_INVERTING, TOIE0_DISABLE, OCIE0_DISABLE};
+	TIMER0_Init(&Timer0Cfg);
+
+
+
+
 	return ES_OK;
 }
 
@@ -40,9 +50,10 @@ ES_t DC_Motor_Stop(DC_MOTOR_Index Motor)
 }
 
 
-ES_t DC_Motor_Move(DC_MOTOR_Index Motor,DC_MOTOR_Cfg MotorCfg)
+ES_t DC_Motor_Move(DC_MOTOR_Index Motor,DC_MOTOR_Dir MotorDir,u8 SpeedOF100)
 {
-	switch(MotorCfg){
+
+	switch(MotorDir){
 	case DC_MOTOR_CW:
 		if(Motor == DC_MOTOR_0){
 			DIO_ES_tSetPinValue(DC_MOTOR_REG, IN1, HIGH);
@@ -64,6 +75,13 @@ ES_t DC_Motor_Move(DC_MOTOR_Index Motor,DC_MOTOR_Cfg MotorCfg)
 	default:
 		return ES_NOT_OK;
 	}
+
+
+	PWM_Set_DutyCycle(SpeedOF100);
+
+
+
+
 	return ES_OK;
 }
 
--- Atmega32-Drivers/HAL/DC_Motor/DC_Motor.h
+++ Atmega32-Drivers/HAL/DC_Motor/DC_Motor.h
@@ -1,22 +1,22 @@
 /*
  * DC_Motor.h
  *
- *  Created on: Jul 23, 2023
+ *  Created on: Aug 3, 2023
  *      Author: Abdallah
  */
 
 #ifndef HAL_DC_MOTOR_DC_MOTOR_H_
 #define HAL_DC_MOTOR_DC_MOTOR_H_
 
-
-// Includes
+#include "../../MCAL/TIMER/Timer.h"
 #include "../../MCAL/Dio/Dio_interface.h"
-#include "../../MCAL/Registers.h"
+
 
 typedef enum{
 	DC_MOTOR_CW,
 	DC_MOTOR_CCW
-}DC_MOTOR_Cfg;
+}DC_MOTOR_Dir;
+
 
 typedef enum{
 	DC_MOTOR_0,
@@ -27,8 +27,7 @@ typedef enum{
 // APIS supported by DC Motor driver
 ES_t DC_Motor_Init(DC_MOTOR_Index Motor);
 
-ES_t DC_Motor_Move(DC_MOTOR_Index Motor,DC_MOTOR_Cfg MotorCfg);
+ES_t DC_Motor_Move(DC_MOTOR_Index Motor,DC_MOTOR_Dir MotorCfg, u8 SpeedOF100);
 ES_t DC_Motor_Stop(DC_MOTOR_Index Motor);
-
 
 #endif /* HAL_DC_MOTOR_DC_MOTOR_H_ */
